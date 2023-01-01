f23e370fe8785d9dd5c37bc326c41289797f3f05
Karen Garcia <saykaren@gmail.com>
2022-12-31 17:55:25 -0700

    adds sky and garden

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
 import './App.css';
+import 'aframe';
+import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader';
+import sky from './assets/stars.jpg';
+import garden from './models/sunday_garden_fun.glb';
 
 function App() {
+  const loader = new GLTFLoader();
+
+  loader.load(garden, (d) => {
+    const entity = document.getElementById("garden");
+    entity.object3D.add(d.scene);
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
- 
-      </header>
-    </div>
-  );
+    <a-scene>
+      <a-assets>
+        <img id="sky" src={sky}/>
+      </a-assets>
+
+      <a-sky
+        color = "#FFFFFF"
+        material="src: #sky"
+        rotation="0 0 0"
+        >
+
+        </a-sky>
+        <a-entity id="garden" position="17 -30 60" scale="15 15 15">
+          
+        </a-entity>
+    </a-scene>
+  )
 }
 
 export default App;
