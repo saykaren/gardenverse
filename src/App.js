644ac60b58cf2993f8d8f5723c4aa528caaf2112
Karen Garcia <saykaren@gmail.com>
2022-12-31 18:12:07 -0700

    adds oculus controls

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import './App.css';
-import 'aframe';
-import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader';
-import sky from './assets/stars.jpg';
-import garden from './models/sunday_garden_fun.glb';
+import "./App.css";
+import "aframe";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import sky from "./assets/stars.jpg";
+import garden from "./models/sunday_garden_fun.glb";
 
 function App() {
   const loader = new GLTFLoader();
@@ -10,26 +10,27 @@ function App() {
   loader.load(garden, (d) => {
     const entity = document.getElementById("garden");
     entity.object3D.add(d.scene);
-  })
+  });
 
   return (
     <a-scene>
       <a-assets>
-        <img id="sky" src={sky}/>
+        <img id="sky" src={sky} />
       </a-assets>
 
-      <a-sky
-        color = "#FFFFFF"
-        material="src: #sky"
-        rotation="0 0 0"
-        >
-
-        </a-sky>
-        <a-entity id="garden" position="17 -30 60" scale="15 15 15">
-          
+      <a-sky color="#FFFFFF" material="src: #sky" rotation="0 0 0"></a-sky>
+      <a-entity id="garden" position="17 -30 60" scale="15 15 15">
+      </a-entity>
+      <a-entity id="rig">
+          <a-camera position="0 1.6 0"></a-camera>
+          <a-entity oculus-touch-controls="hand: left"></a-entity>
+          <a-entity
+            oculus-touch-controls="hand: right"
+            oculus-thumbstick-controls
+          ></a-entity>
         </a-entity>
     </a-scene>
-  )
+  );
 }
 
 export default App;
